077d49fbffd0c6dace0d4d119a85e5a6408c4495
tgnc <tgnc@yandex-team.ru>
2021-08-21 22:52:05 +0300

    tuning eslint rules

--- src/client.tsx
+++ src/client.tsx
@@ -1,15 +1,15 @@
-import App from './App';
+import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
 import { hydrate } from 'react-dom';
+import App from './App';
 
 hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'),
 );
 
 if (module.hot) {
-  module.hot.accept();
+    module.hot.accept();
 }
